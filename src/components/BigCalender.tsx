27d7a4ba0093c72505dc56adf555eba7732360a2
mohamedo396 <mohamadamrani83@gmail.com>
2025-02-28 08:46:59 +0000

    The admin layout

---
+++ src/components/BigCalender.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BigCalender = () => {
+  return (
+    <div>BigCalender</div>
+  )
+}
+
+export default BigCalender
---
+++ src/components/UseCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UseCard = () => {
+  return (
+    <div>UseCard</div>
+  )
+}
+
+export default UseCard
