70de615ca4cbdb3630999b10f4118219ca1a1d03
mohamedo396 <mohamadamrani83@gmail.com>
2025-03-02 13:26:30 +0000

    finishing the teacher list

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['images.pexels.com'], // Allow images from Pexels
+    },
+  };
+  
+  export default nextConfig;
+  
